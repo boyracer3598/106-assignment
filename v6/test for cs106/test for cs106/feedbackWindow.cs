a39caee00b23074de55c417cd6d41135cab01501
Cole <poby19@gmail.com>
2024-11-18 09:57:55 +1300

    version 6

---
+++ v6/test for cs106/test for cs106/feedbackWindow.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace test_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class feedbackWindow : Window
+    {
+        
+
+    }
+}
+
