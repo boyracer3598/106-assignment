d84f27a12a7ee5416c495076c599a9a3afcd177b
Yoshiaroo1 <270136550@yoobeestudent.ac.nz>
2024-11-21 10:20:41 +1300

    v8

---
+++ v8/test for cs106/test for cs106/feedbackWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace test_2
+{
+
+    public partial class feedbackWindow : Window
+    {
+        public feedbackWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ELostL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "Search";
+            }
+        }
+
+        private void EGainedL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "Search")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "";
+            }
+        }
+
+        private void FeedbackC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void NotificationsC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+    }
+}
+
