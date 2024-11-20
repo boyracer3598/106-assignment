2f7347847c43b0f2a3faf48a58b9dbc545d7e2c7
Yoshiaroo1 <270136550@yoobeestudent.ac.nz>
2024-11-21 09:14:34 +1300

    v7

---
+++ v7/test for cs106/test for cs106/AdminWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace test_2
+{
+
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+            Loadtext();
+        }
+
+        public void Loadtext()
+        {
+            try
+            {
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath += "data\\Accounts.csv";
+
+                MessageBox.Show(filename);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+    }
+}
+
---
+++ v7/test for cs106/test for cs106/AssingningWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace test_2
+{
+
+    public partial class AssingningWindow : Window
+    {
+        public AssingningWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dashboardC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void FeedbackC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void InboxC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void NotificationsC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void TicketC(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void ELostL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "Search";
+            }
+        }
+
+        private void EGainedL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "Search")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "";
+            }
+        }
+
+    }
+}
+
