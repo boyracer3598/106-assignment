3e2e54b6f820321e78e23cde6ee288867bc7d6f8
Yoshiaroo1 <270136550@yoobeestudent.ac.nz>
2024-11-28 13:17:34 +1300

    final version

---
+++ test for cs106/test for cs106/Login.xaml.cs
@@ -0,0 +1,261 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace test_2
+{
+    
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public static string currentUser;
+        public static string currentUserN;
+        public static string currentUserFirst;
+        public static bool Admin = false;
+        public static bool Op = false;
+
+        private void GoAccount(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.Show();
+            Application.Current.Windows.OfType<Login>().SingleOrDefault()?.Close();
+        }
+        
+       
+
+        
+
+        
+
+        
+
+        
+
+        
+        private void ELostL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "Email";
+            }
+        }
+
+        private void EGainedL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "Email")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "";
+            }
+        }
+
+        private void PLostL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "Password";
+            }
+        }
+
+        private void PGainedL(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the textbox
+            string text = ((TextBox)sender).Text;
+
+            // If it is empty, set the text to "Enter your username"
+            if (text == "Password")
+            {
+                // If it is empty, set the text to "First name"
+                ((TextBox)sender).Text = "";
+            }
+        }
+
+        
+
+        
+
+        string User;
+        string Pass;
+        bool noUser;
+        private void StartLoginL(object sender, RoutedEventArgs e)
+        {
+            
+            
+
+            try
+            {
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath += "data\\Accounts.csv";
+                Trace.WriteLine(filename);
+                MainWindow.CleanFile(filename);
+                List<(string, List<string>)> csvData;
+
+                User = Email.Text;
+                Pass = Password.Text;
+
+                csvData = CsvReaderL.ReadCsv(filename);
+
+                // Check if there is a second row
+                if (csvData.Count >= 1)
+                {
+                    for (int i = 0; i < csvData.Count; i++)
+                    {
+                        var Row = csvData[i]; // Index 1 for the second row
+
+                        if (Row.Item2[3] == User && Row.Item2[2] == Pass)
+                        {
+                            currentUser = Row.Item1 + "\n" + Row.Item2[1];
+                            currentUserN = Row.Item1 + " " + Row.Item2[1];
+                            currentUserFirst = Row.Item1;
+                            if (Row.Item2[4] == "0")
+                            {
+                                Op = false;
+                                Admin = false;
+                            }
+                            else
+                            if (Row.Item2[4] == "2")
+                            {
+                                Admin = true;
+                                MessageBox.Show("Admin login success");
+                            }
+                            else if (Row.Item2[4] == "1")
+                            {
+                                Admin = false;
+                                Op = true;
+                                MessageBox.Show("Operator login success");
+                            }
+                            noUser = false; break;
+                            
+                        }
+                        else
+                        {
+                            noUser= true;
+                        }
+                    }
+
+                    if (noUser == false && Admin == false && Op == false)
+                    {
+                        MessageBox.Show("login success");
+                        System.Threading.Thread.Sleep(50);
+                        TicketWindow ticketsubmissionwindow = new TicketWindow();
+                        ticketsubmissionwindow.Show();
+                        Application.Current.Windows.OfType<Login>().SingleOrDefault()?.Close();
+
+                    }
+                    else if (noUser == true && Admin == false)
+                    { 
+                        MessageBox.Show("login failed");
+                        Op = false;
+                        Admin = false;
+                    }
+
+                    if (Admin == true)
+                    {
+                        System.Threading.Thread.Sleep(50);
+                        AdminWindow adminwindow = new AdminWindow();
+                        adminwindow.Show();
+                        Application.Current.Windows.OfType<Login>().SingleOrDefault()?.Close();
+                    }
+                    if (Op == true)
+                    {
+                        System.Threading.Thread.Sleep(50);
+                        OperatorWindow Opwindow = new OperatorWindow();
+                        Opwindow.Show();
+                        Application.Current.Windows.OfType<Login>().SingleOrDefault()?.Close();
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("The CSV file does not have a second row.");
+                    MessageBox.Show("there is no second row");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+        }
+
+        private void FinishProgram(object sender, RoutedEventArgs e)
+        {
+            // Close the program
+            Close();
+        }
+
+        private void windowHide()
+        {
+
+        }
+    }
+
+    
+
+    public class CsvReaderL
+    {
+        public static List<(string, List<string>)> ReadCsv(string filename)
+        {
+            var result = new List<(string, List<string>)>();
+
+            using (var file = new StreamReader(filename))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var values = new List<string>();
+                    var cells = line.Split(',');
+
+                    foreach (var cell in cells)
+                    {
+                        values.Add(cell);
+                    }
+
+                    // Ensure there's at least one value before adding to the result
+                    if (values.Count > 0)
+                    {
+                        result.Add((values[0], values));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ test for cs106/test for cs106/OperatorWindow.xaml.cs
@@ -0,0 +1,366 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.CompilerServices;
+
+namespace test_2
+{
+    
+    
+
+    public partial class OperatorWindow : Window
+    {
+        int tikselect = 1;
+        int OpSelect = 1;
+        string OpSelectname = "";
+        string Op1name = "";
+        string Op2name = "";
+        string Op3name = "";
+
+
+        
+
+        public OperatorWindow()
+        {
+            InitializeComponent();
+
+            LoadText();
+
+            
+
+            
+        }
+
+        private void LoadText()
+        {
+            try
+            {
+                //get the file path for the accounts file and clean it
+                //then read the file and get the data
+                //then loop through the data and get the second ro
+
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename2 = newPath += "data\\Accounts.csv";
+                MainWindow.CleanFile(filename2);
+
+                List<(string, List<string>)> csvData2;
+
+                csvData2 = CsvReaderL.ReadCsv(filename2);
+
+                if (csvData2.Count >= 1)
+                {
+                    int count = 0;
+                    for (int i = 0; i < csvData2.Count; i++)
+                    {
+                        var Row = csvData2[i];
+
+                        if (Row.Item2[4] == "1")
+                        {
+                            count++;
+                            Op1.Content = Row.Item1 + " " + Row.Item2[1];
+                            Op1name = Row.Item1 + " " + Row.Item2[1];
+
+                            if (count == 1 && Row.Item2[4] == "1")
+                            {
+                                Op2.Content = Row.Item1 + " " + Row.Item2[1];
+                                Op2name = Row.Item1 + " " + Row.Item2[1];
+
+                            }
+                            if (count == 2 && Row.Item2[4] == "1")
+                            {
+                                Op3.Content = Row.Item1 + " " + Row.Item2[1];
+                                Op3name = Row.Item1 + " " + Row.Item2[1];
+                            }
+                            if (count == 3)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+
+            try
+            {
+                Usertext.Content = Login.currentUser;
+                
+               
+
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath += "data\\Tickets.csv";
+
+                List<(string, List<string>)> csvData;
+                
+
+                csvData = CsvReaderL.ReadCsv(filename);
+
+                if (csvData.Count >= 1)
+                {
+                    for (int i = 0; i < csvData.Count; i++)
+                    {
+                        var Row = csvData[i];
+
+                        if (i == 0)
+                        {
+                            description.Text = Row.Item2[2];
+                            tik1text.Content = Row.Item1;
+                        }
+
+                        if (i == 1)
+                        {
+                            tik2text.Content = Row.Item1;
+                        }
+
+                        if (i == 2)
+                        {
+                            tik3text.Content = Row.Item1;
+                        }   
+                    }
+                }
+
+               
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+       
+
+        
+        private void Assign(object sender, EventArgs e)
+        {
+            //reads the tickets file and gets the data
+            MessageBox.Show("Ticket assigned");
+            try
+            {
+                if (OpSelect == 1)
+                {
+                    OpSelectname = Op1name;
+                }
+                if (OpSelect == 2)
+                {
+                    OpSelectname = Op2name;
+                }
+                if (OpSelect == 3)
+                {
+                    OpSelectname = Op3name;
+                }
+
+
+
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath + "data\\Tickets.csv";
+                string filename2 = newPath + "data\\assigned.csv";
+                MainWindow.CleanFile(filename);
+                MainWindow.CleanFile(filename2);
+                List<(string, List<string>)> csvData;
+
+                csvData = CsvReaderL.ReadCsv(filename);
+
+                if (csvData.Count >= 1)
+                {
+                    for (int i = 0; i < csvData.Count; i++)
+                    {
+                        var Row = csvData[i];
+
+                        //then loops through the data and gets to the selected ticket
+                        //then writes the Operator name to the ticket row in the file
+                        if (i == tikselect - 1)
+                        {
+                            //Row.Item2[7] = OpSelectname;
+                            //then writes the data to the temp file
+                            string csvInfo = Row.Item1 + ',' + Row.Item2[1] + ',' + Row.Item2[2] + ',' + Row.Item2[3] + ',' + Row.Item2[4] + ',' + Row.Item2[5] + ',' + Row.Item2[6] + ',' + OpSelectname + '\n';
+                            File.AppendAllText(filename2, csvInfo);
+                        }
+
+                        
+
+
+                    }
+                }
+
+            
+                
+
+
+               
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+
+
+
+        }
+
+        private void tik1click(object sender, EventArgs e)
+        {
+            tikselect = 1;
+            try
+            {
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath += "data\\Tickets.csv";
+                MainWindow.CleanFile(filename);
+                List<(string, List<string>)> csvData;
+
+                csvData = CsvReaderL.ReadCsv(filename);
+
+                if (csvData.Count >= 1)
+                {
+                    for (int i = 0; i < csvData.Count; i++)
+                    {
+                        var Row = csvData[i];
+
+                        if (i == 0)
+                        {
+                            description.Text = Row.Item2[2];
+                            tik1text.Content = Row.Item1;
+                            typebox.Text = Row.Item2[1];
+                            UserT.Content = Row.Item2[6];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+        private void tik1click2(object sender, EventArgs e)
+        {
+
+            tikselect = 2;
+            try
+            {
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath += "data\\Tickets.csv";
+                MainWindow.CleanFile(filename);
+                List<(string, List<string>)> csvData;
+
+                csvData = CsvReaderL.ReadCsv(filename);
+
+                if (csvData.Count >= 1)
+                {
+                    for (int i = 0; i < csvData.Count; i++)
+                    {
+                        var Row = csvData[i];
+
+                        if (i == 1)
+                        {
+                            description.Text = Row.Item2[2];
+                            tik2text.Content = Row.Item1;
+                            typebox.Text = Row.Item2[1];
+                            UserT.Content = Row.Item2[6];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+        private void tik1click3(object sender, EventArgs e)
+        {
+            tikselect = 3;
+            try
+            {
+                string appCurrentFile = System.AppDomain.CurrentDomain.BaseDirectory;
+                Trace.WriteLine(appCurrentFile);
+                string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(appCurrentFile, @""));
+                string filename = newPath += "data\\Tickets.csv";
+                MainWindow.CleanFile(filename);
+                List<(string, List<string>)> csvData;
+
+                csvData = CsvReaderL.ReadCsv(filename);
+
+                if (csvData.Count >= 1)
+                {
+                    for (int i = 0; i < csvData.Count; i++)
+                    {
+                        var Row = csvData[i];
+
+                        if (i == 2)
+                        {
+                            description.Text = Row.Item2[2];
+                            tik3text.Content = Row.Item1;
+                            typebox.Text = Row.Item2[1];
+                            UserT.Content = Row.Item2[6];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+        private void Op1Ass(object sender, RoutedEventArgs e)
+        {
+            OpSelect = 1;
+            MessageBox.Show("Operator 1 selected");
+        }
+
+        private void Op2Ass(object sender, RoutedEventArgs e)
+        {
+            OpSelect = 2;
+            MessageBox.Show("Operator 2 selected");
+        }
+
+        private void Op3Ass(object sender, RoutedEventArgs e)
+        {
+            OpSelect = 3;
+            MessageBox.Show("Operator 3 selected");
+        }
+
+        private void backButton(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
+
